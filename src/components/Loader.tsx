923714aa42560430eb4d3290e68441aab5c5cf48
destolyar <dead.stolyar@gmail.com>
2023-03-12 21:37:48 +0300

    loader update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,15 +2,9 @@ import styles from '../styles/Loader.module.scss'
 
 export const Loader = () => {
   return (
-    <div className={styles.ldsRoller}>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className={styles.container}>
+      <img className={styles.image} src='/signs/horoscope.png'/>
+      <div className={styles.ldsDualRing}/>
     </div>
   )
 }
