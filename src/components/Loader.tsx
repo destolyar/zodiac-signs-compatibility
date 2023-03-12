544a40f60052fbc5ed4d470b7ee65aafb0ee0855
destolyar <dead.stolyar@gmail.com>
2023-03-12 23:41:34 +0300

    image optimization

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import styles from '../styles/Loader.module.scss'
 
 export const Loader = () => {
   return (
     <div className={styles.container}>
-      <img className={styles.image} src='/signs/horoscope.png'/>
+      <Image className={styles.image} src='/signs/horoscope.png' alt={"horoscope"} width={120} height={120}/>
       <div className={styles.ldsDualRing}/>
     </div>
   )
