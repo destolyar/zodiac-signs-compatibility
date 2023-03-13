213a3a1c522a5bbb38a6d61edb8c801cffa8b232
destolyar <dead.stolyar@gmail.com>
2023-03-13 14:39:32 +0300

    background, fonts

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import styles from "../styles/Sign.module.scss"
 
 export const Sign = ({ signInfo }: { signInfo: SignInfo }) => {
@@ -5,7 +6,7 @@ export const Sign = ({ signInfo }: { signInfo: SignInfo }) => {
 
   return (
     <div className={styles.container}>
-      <img className={styles.image} src={imagePath} alt={name} />
+      <Image className={styles.image} src={imagePath} alt={name} loading="lazy" width={128} height={128}/>
       <h3 className={styles.title}>{name}</h3>
       <p className={styles.dates}>{dates}</p>
     </div>
