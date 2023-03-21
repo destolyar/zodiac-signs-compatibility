13a96f3cacac76e890a90d1a90e365eedc1db027
destolyar <dead.stolyar@gmail.com>
2023-03-21 16:05:04 +0300

    build update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/static/(.*)\\.js',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript',
+          },
+        ],
+      },
+    ];
+  },
+  assetPrefix: './'
 }
 
 module.exports = nextConfig
